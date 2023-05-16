8bb911e0a0ae307625e43a03d510e66b0c86383d
hasic-haris <haris.hasic@elix-inc.com>
2023-05-16 21:20:57 +0900

    Update the 'utilities' package.

--- chemical_reaction_data/utilities/archive/extraction.py
+++ chemical_reaction_data/utilities/archive/extraction.py
@@ -8,52 +8,6 @@ class ArchiveExtractionUtilities:
     """ The archive extraction utilities class. """
 
     @staticmethod
-    def extract_from_zip_archive(
-            archive_file_path: str,
-            output_directory_path: str,
-            archive_file_content_base_paths: Iterable[str] = None,
-            enable_logger: bool = False
-    ) -> None:
-        """
-        Extract the contents from a '*.zip' archive file.
-
-        :parameter archive_file_path: The path to the archive file.
-        :parameter output_directory_path: The path to the directory where the extracted archive file contents should be
-                                          stored.
-        :parameter archive_file_content_base_paths: The base paths of the archive file contents that should be
-                                                    extracted.
-        :parameter enable_logger: The indicator whether the logger should be enabled.
-        """
-
-        try:
-            from zipfile import ZipFile
-
-            with ZipFile(archive_file_path) as archive:
-                if archive_file_content_base_paths is None:
-                    archive.extractall(
-                        path=output_directory_path
-                    )
-
-                else:
-                    for archive_file_content_path in archive.namelist():
-                        if any(
-                            archive_file_content_path.startswith(archive_file_content_base_path)
-                            for archive_file_content_base_path in archive_file_content_base_paths
-                        ):
-                            archive.extract(
-                                member=archive_file_content_path,
-                                path=output_directory_path
-                            )
-
-        except Exception as exception_handle:
-            if enable_logger:
-                getLogger(
-                    "{0}.ArchiveExtractionUtilities.extract_from_zip_archive".format(__name__)
-                ).exception(exception_handle)
-
-            raise
-
-    @staticmethod
     def extract_from_7z_archive(
             archive_file_path: str,
             output_directory_path: str,
@@ -148,6 +102,52 @@ class ArchiveExtractionUtilities:
             if enable_logger:
                 getLogger(
                     "{0}.ArchiveExtractionUtilities.extract_from_tar_gz_archive".format(__name__)
+                ).exception(exception_handle)
+
+            raise
+
+    @staticmethod
+    def extract_from_zip_archive(
+            archive_file_path: str,
+            output_directory_path: str,
+            archive_file_content_base_paths: Iterable[str] = None,
+            enable_logger: bool = False
+    ) -> None:
+        """
+        Extract the contents from a '*.zip' archive file.
+
+        :parameter archive_file_path: The path to the archive file.
+        :parameter output_directory_path: The path to the directory where the extracted archive file contents should be
+                                          stored.
+        :parameter archive_file_content_base_paths: The base paths of the archive file contents that should be
+                                                    extracted.
+        :parameter enable_logger: The indicator whether the logger should be enabled.
+        """
+
+        try:
+            from zipfile import ZipFile
+
+            with ZipFile(archive_file_path) as archive:
+                if archive_file_content_base_paths is None:
+                    archive.extractall(
+                        path=output_directory_path
+                    )
+
+                else:
+                    for archive_file_content_path in archive.namelist():
+                        if any(
+                            archive_file_content_path.startswith(archive_file_content_base_path)
+                            for archive_file_content_base_path in archive_file_content_base_paths
+                        ):
+                            archive.extract(
+                                member=archive_file_content_path,
+                                path=output_directory_path
+                            )
+
+        except Exception as exception_handle:
+            if enable_logger:
+                getLogger(
+                    "{0}.ArchiveExtractionUtilities.extract_from_zip_archive".format(__name__)
                 ).exception(exception_handle)
 
             raise
